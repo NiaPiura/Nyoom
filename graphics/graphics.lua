bdc404d774d0d8f4387da282800a542fd1d4b53b
NiaPiura <nia@niapiura.dev>
2025-04-16 01:20:54 +0200

    Added implementation of Text Object

--- graphics/graphics.lua
+++ graphics/graphics.lua
@@ -1,4 +1,5 @@
-local newColor = require ('nyoom.graphics.color')
+local newColor = require('nyoom.graphics.color')
+local newText = require('nyoom.graphics.text')
 
 ---@class Graphics
 local graphics = {}
@@ -11,6 +12,17 @@ local graphics = {}
 ---@return Color
 function graphics.newColor(r, g, b, a)
   return newColor(r, g, b, a)
+end
+
+---Creates an object representing a color.
+---@param string string
+---@param x number
+---@param y number
+---@param width number
+---@param height number
+---@return Text
+function graphics.newText(string, x, y, width, height)
+  return newText(string, x, y, width, height)
 end
 
 return graphics
--- graphics/text.lua
+++ graphics/text.lua
@@ -1,24 +1,101 @@
+local newColor = require('nyoom.graphics.color')
+
 ---@class Text
 ---@field string string
 ---@field font love.Font
 ---@field rect Rect
+---@field color Color
+---@field horizontalAlignment TextHorizontalAlignment
+---@field verticalAlignment TextVerticalAlignment
+---@field offset number
+---
+---@field draw fun(self: Text)
+---@field setFont fun(self: Text, font: love.Font): Text
+---@field setColor fun(self: Text, color: Color): Text
+---@field setAlignments fun(self: Text, horizonral: TextHorizontalAlignment, vertical: TextVerticalAlignment): Text
+---@field setPosition fun(self: Text, x: number, y: number): Text
+---@field setPosition fun(self: Text, position: Vector2): Text
+---@field setSize fun(self: Text, width: number, height: number): Text
+---@field setSize fun(self: Text, size: Vector2): Text
+---@field updateText fun(self: Text)
+
+---@alias TextHorizontalAlignment 'left'|'right'|'center'|'justify'
+---@alias TextVerticalAlignment 'top'|'bottom'|'center'
+
+local defaults = {
+  color = newColor(1, 1, 1),
+  font = love.graphics.newFont(12)
+}
 
 local methods, metamethods = {}, { __name = 'Text' }
 
-local function newText(string, x, y, width, height, font)
+---@param string string
+---@param x number
+---@param y number
+---@param width number
+---@param height number
+---@return Text
+local function newText(string, x, y, width, height)
   local text = {
     string = string,
     rect = nyoom.objects.newRect(x, y, width, height),
-    font = font or love.graphics.newFont(12),
+    font = defaults.font,
+    color = defaults.color,
+    horizontalAlignment = 'left',
+    verticalAlignment = 'top',
+    offset = 0
   }
 
   setmetatable(text, metamethods)
 
+  text:updateText()
+
   return text
 end
 
+---@param self Text
 function methods:draw()
+  love.graphics.setColor(self.color)
+  love.graphics.printf(self.string, self.font, self.rect.x, self.rect.y + self.offset, self.rect.width, self.horizontalAlignment)
+end
 
+function methods:setFont(font)
+  self.font = font
+  self:updateText()
+  return self
+end
+
+function methods:setColor(color)
+  self.color = color
+  return self
+end
+
+function methods:setAlignments(horizontal, vertical)
+  self.horizontalAlignment = horizontal
+  self.verticalAlignment = vertical
+  self:updateText()
+  return self
+end
+
+---@param x number|Vector2
+---@param y number|nil
+function methods:setPosition(x, y)
+  if type(x) == 'number' then self.rect.position = nyoom.objects.newVector2(x, y)
+  else self.rect.position = x end
+end
+
+---@param self Text
+---@param width number|Vector2
+---@param height number|nil
+function methods:setSize(width, height)
+  if type(width) == 'number' then self.rect.size = nyoom.objects.newVector2(width, height)
+  else self.rect.size = width end
+  self:updateText()
+end
+
+---@param self Text
+function methods:updateText()
+  self.offset = (self.font:getHeight() - self.rect.height) / 2
 end
 
 -- Metamethods
