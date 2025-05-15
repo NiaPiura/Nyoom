5b31f41d80936c82b08e2caf85d7a1c8e32189cd
NiaPiura <nia@niapiura.dev>
2025-05-16 00:34:03 +0200

    Added TileGrid, using a TileSet to render a Grid

--- graphics/graphics.lua
+++ graphics/graphics.lua
@@ -1,11 +1,13 @@
 local newColor = require('nyoom.graphics.color')
 local newText = require('nyoom.graphics.text')
 local newTileSet = require('nyoom.graphics.tileSet')
+local newTileGrid = require('nyoom.graphics.tileGrid')
 
 ---@class Nyoom.Graphics
 ---@field newColor fun(r: number, g: number, b: number, a?: number): Nyoom.Color Creates an object representing a color.
 ---@field newText fun(string: string, x: number, y: number, width: number, height: number): Nyoom.Text Creates an object representing a color.
 ---@field newTileSet fun(image: love.Image, tileWidth: number, tileHeight: number): Nyoom.TileSet
+---@field newTileGrid fun(tileSet: Nyoom.TileSet, width: integer, height: integer): Nyoom.TileGrid
 local graphics = {}
 
 function graphics.newColor(r, g, b, a)
@@ -18,6 +20,10 @@ end
 
 function graphics.newTileSet(image, tileWidth, tileHeight)
   return newTileSet(image, tileWidth, tileHeight)
+end
+
+function graphics.newTileGrid(tileSet, width, height)
+  return newTileGrid(tileSet, width, height)
 end
 
 return graphics
---
+++ graphics/tileGrid.lua
@@ -0,0 +1,100 @@
+--- A renderable `Nyoom.Grid` using `love.SpriteBatch` and `Nyoom.TileSet` to determine how and what to render. 
+---@class Nyoom.TileGrid
+---@field width integer
+---@field height integer
+---@field size Nyoom.Vector2
+---@field tileSet Nyoom.TileSet
+---@field tileMap Nyoom.Grid
+---@field spriteMap Nyoom.Grid
+---@field spriteBatch love.SpriteBatch
+---
+---@field setSize fun(self: Nyoom.TileGrid, width: integer, height: integer): Nyoom.TileGrid Resizes the tilegrid.
+---@field setTile fun(self: Nyoom.TileGrid, width: integer, height: integer, id: integer): Nyoom.TileGrid
+---@field setTileSet fun(self: Nyoom.TileGrid, tileSet: Nyoom.TileSet): Nyoom.TileGrid
+---@field getTilePosition fun(self: Nyoom.TileGrid, index: integer): Nyoom.Vector2?
+
+local methods, metamethods = {}, { __name = 'Nyoom.TileGrid' }
+
+---@param self Nyoom.TileGrid
+local function repopulateSpriteBatch(self)
+  self.spriteMap:clear()
+  self.spriteBatch:clear()
+  for i, id in ipairs(self.tileMap.map) do
+    local pos = self.tileMap:getPosition(i)
+    local tilePos = self:getTilePosition(i)
+    if pos and tilePos then
+      local spriteIndex = self.spriteBatch:add(self.tileSet.quads[id], tilePos.x, tilePos.y)
+      self.spriteMap:setValue(pos.x, pos.y, spriteIndex)
+    end
+  end
+end
+
+---@param tileSet Nyoom.TileSet
+---@param width integer
+---@param height integer
+---@return Nyoom.TileGrid
+local function newTileGrid(tileSet, width, height)
+  local tileGrid = {
+    tileSet = tileSet,
+    tileMap = nyoom.common.newGrid(width, height),
+    spriteMap = nyoom.common.newGrid(width, height),
+    spriteBatch = love.graphics.newSpriteBatch(tileSet.image)
+  }
+
+  setmetatable(tileGrid, metamethods)
+
+  repopulateSpriteBatch(tileGrid)
+
+  return tileGrid
+end
+
+-- Methods
+
+function methods:setSize(width, height)
+  self.tileMap:setSize(width, height)
+  self.spriteMap:setSize(width, height)
+  repopulateSpriteBatch(self)
+
+  return self
+end
+
+function methods:setTile(x, y, id)
+  local index = self.tileMap:getIndex(x, y)
+  local spriteIndex = self.spriteMap:getValue(x, y)
+  if not index then return end
+
+  self.tileMap:setValue(x, y, id)
+  self.spriteBatch:set(spriteIndex, self.tileSet.quads[id], x * self.tileSet.tileWidth, y * self.tileSet.tileHeight)
+  return self
+end
+
+function methods:setTileSet(tileSet)
+  self.tileSet = tileSet
+  self.spriteBatch:setTexture(tileSet.image)
+  repopulateSpriteBatch(self)
+  return self
+end
+
+function methods:getTilePosition(index)
+  local pos = self.tileMap:getPosition(index)
+  if not pos then return
+  else return nyoom.common.newVector2(pos.x * self.tileSet.tileWidth, pos.y * self.tileSet.tileHeight) end
+end
+
+-- Metamethods
+
+function metamethods:__index(key)
+  if key == 'width' then return self.tileMap.width end
+  if key == 'height' then return self.tileMap.height end
+  if key == 'size' then return self.tileMap.size end
+  return methods[key]
+end
+
+function metamethods:__newindex(key, value)
+  if key == 'width' then self:setSize(value, self.tileMap.height)
+  elseif key == 'height' then self:setSize(self.tileMap.width, value)
+  elseif key == 'size' then self:setSize(value.x, value.y)
+  else rawset(self, key, value) end
+end
+
+return newTileGrid
