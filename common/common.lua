01a6f11645b50160b20461b1028b48d3bc8168fc
NiaPiura <nia@niapiura.dev>
2025-05-15 21:36:23 +0200

    Added Grid to common library

--- common/common.lua
+++ common/common.lua
@@ -1,9 +1,11 @@
 local newVector2 = require('nyoom.common.vector2')
 local newRect = require('nyoom.common.rect')
+local newGrid = require('nyoom.common.grid')
 
 ---@class Nyoom.Common
----@field newVector2 fun(x?: number, y?: number): Nyoom.Vector2 Create a new Vector.
----@field newRect fun(x?: number, y?: number, width?: number, height?: number): Nyoom.Rect Create a new Rect.
+---@field newVector2 fun(x?: number, y?: number): Nyoom.Vector2
+---@field newRect fun(x?: number, y?: number, width?: number, height?: number): Nyoom.Rect
+---@field newGrid fun(width: number, height: number, defaultValue?: number): Nyoom.Grid
 local common = {}
 
 function common.newVector2(x, y)
@@ -12,6 +14,10 @@ end
 
 function common.newRect(x, y, width, height)
   return newRect(x, y, width, height)
+end
+
+function common.newGrid(width, height, defaultValue)
+  return newGrid(width, height, defaultValue)
 end
 
 
