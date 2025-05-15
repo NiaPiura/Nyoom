fe895d874cc6e172220c247e9ee022004d546068
NiaPiura <nia@niapiura.dev>
2025-05-16 00:33:00 +0200

    Update Typing and allow some methods to be chained

--- common/grid.lua
+++ common/grid.lua
@@ -1,42 +1,30 @@
 --- A 2D grid of values. Positions are 0-indexed while `map` is 1-indexed
 ---@class Nyoom.Grid
----@field size Nyoom.Vector2
 ---@field width integer
 ---@field height integer
+---@field size Nyoom.Vector2
 ---@field defaultValue any
 ---@field map any[]
 ---
 ---@field getPosition fun(self: Nyoom.Grid, index: integer): Nyoom.Vector2? Gets the index in the map that correlates to the given position, or nil if out of bounds.
 ---@field getIndex fun(self: Nyoom.Grid, x: integer, y: integer): integer? Gets the index in the map that correlates to the given position, or nil if out of bounds.
----@field setValue fun(self: Nyoom.Grid, x: integer, y: integer, value: any): Nyoom.Grid Sets a value mapped to given position. Can be chained.
 ---@field getValue fun(self: Nyoom.Grid, x: integer, y: integer): any Gets a value mapped to given position, or nil if out of bounds.
----@field setSize fun(self: Nyoom.Grid, width: integer, height: integer) Resizes the grid.
----@field clear fun(self: Nyoom.Grid, value: any) Clear the grid, replacing values with either given value, or `defaultValue`.
+---@field setValue fun(self: Nyoom.Grid, x: integer, y: integer, value: any): Nyoom.Grid Sets a value mapped to given position. Can be chained.
+---@field setSize fun(self: Nyoom.Grid, width: integer, height: integer): Nyoom.Grid Resizes the grid.
+---@field clear fun(self: Nyoom.Grid, value: any): Nyoom.Grid Clear the grid, replacing values with either given value, or `defaultValue`.
 
 local methods, metamethods = {}, { __name = 'Nyoom.Grid' }
 
----@param index integer
----@param width integer
----@return integer
----@return integer
 local function indexToPosition(index, width)
   return (index-1) % width, math.floor((index-1) / width)
 end
 
----@param x integer
----@param y integer
----@param width integer
----@return integer
 local function positionToIndex(x, y, width)
   return (width * y) + x + 1
 end
 
----@param width integer
----@param height integer
----@return integer
----@return integer
 local function clampDimensions(width, height)
-  return math.max(width, 1), math.max(height, 1)
+  return math.max(width, 0), math.max(height, 0)
 end
 
 ---@param width integer
@@ -59,32 +47,27 @@ end
 
 -- Methods
 
----@param self Nyoom.Grid
 function methods:getPosition(index)
   if index < 1 or index > #self.map then return end
   return nyoom.common.newVector2(indexToPosition(index, self.width))
 end
 
----@param self Nyoom.Grid
 function methods:getIndex(x, y)
   if x < 0 or x > self.width - 1 or y < 0 or y > self.height - 1 then return end
   return positionToIndex(x, y, self.width)
 end
 
----@param self Nyoom.Grid
 function methods:setValue(x, y, value)
   local index = self:getIndex(x, y)
   if index then self.map[index] = value end
   return self
 end
 
----@param self Nyoom.Grid
 function methods:getValue(x, y)
   local index = self:getIndex(x, y)
   return self.map[index]
 end
 
----@param self Nyoom.Grid
 function methods:setSize(width, height)
   width, height = clampDimensions(width, height)
   local map = {}
@@ -98,14 +81,15 @@ function methods:setSize(width, height)
 
   self.size = nyoom.common.newVector2(width, height)
   self.map = map
+  return self
 end
 
----@param self Nyoom.Grid
 function methods:clear(value)
   value = value or self.defaultValue
   for i = 1, self.width * self.height do
     self.map[i] = value
   end
+  return self
 end
 
 -- Metamethods
