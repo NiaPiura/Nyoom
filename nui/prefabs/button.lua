3abc46bce075fcf2ed3110dca20d6e74ff23b2a5
NiaPiura <nia@niapiura.dev>
2025-05-15 20:35:04 +0200

    Fixed missing update to changed accesssor token

--- nui/prefabs/button.lua
+++ nui/prefabs/button.lua
@@ -7,7 +7,7 @@ local function setColor(element, color)
 end
 
 local function newButton(text, x, y, width, height, parent, buttonColor, textColor)
-  local element = nyoom.nui.newElement('button' .. text, x, y, width, height, parent)
+  local element = nyoom.ui.newElement('button' .. text, x, y, width, height, parent)
   local textObject = nyoom.graphics.newText(text, 0, 0, width, height):setAlignments('center', 'center'):setColor(textColor or nyoom.graphics.newColor(1, 1, 1))
   buttonColor = buttonColor or nyoom.graphics.newColor(0.3, 0.3, 0.3)
 
