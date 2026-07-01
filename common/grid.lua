1d147962d2a88a73e38946734001b8659e86cf33
NiaPiura <nia@niapiura.dev>
2026-07-01 12:05:57 +0200

    Added support for using vectors directly in Grid method params

--- common/grid.lua
+++ common/grid.lua
@@ -8,9 +8,12 @@
 ---
 ---@field getPosition fun(self: Nyoom.Grid, index: integer): Nyoom.Vector2? Returns the position in the map that correlates to the given index, or nil if out of bounds.
 ---@field getIndex fun(self: Nyoom.Grid, x: integer, y: integer): integer? Returns the index in the map that correlates to the given position, or nil if out of bounds.
+---@field getIndex fun(self: Nyoom.Grid, position: Nyoom.Vector2): integer? Returns the index in the map that correlates to the given position, or nil if out of bounds.
 ---@field getValue fun(self: Nyoom.Grid, x: integer, y: integer): any Returns a value mapped to given position, or nil if out of bounds.
+---@field getValue fun(self: Nyoom.Grid, position: Nyoom.Vector2): any Returns a value mapped to given position, or nil if out of bounds.
 ---@field getValue fun(self: Nyoom.Grid, index: integer): any Returns a value mapped to a given index, or nil if out of bounds.
----@field setValue fun(self: Nyoom.Grid, x: integer, y: integer, value: any): Nyoom.Grid Sets a value mapped to given position. Can be chained.
+---@field setValue fun(self: Nyoom.Grid, value: any, x: integer, y: integer): Nyoom.Grid Sets a value mapped to given position. Can be chained.
+---@field setValue fun(self: Nyoom.Grid, value: any, position: Nyoom.Vector2): Nyoom.Grid Sets a value mapped to given position. Can be chained.
 ---@field setValue fun(self: Nyoom.Grid, value: any, index: integer): Nyoom.Grid Sets a value mapped to given index. Can be chained.
 ---@field setSize fun(self: Nyoom.Grid, width: integer, height: integer): Nyoom.Grid Non-destructively resizes the grid.
 ---@field clear fun(self: Nyoom.Grid, value: any): Nyoom.Grid Clear the grid, replacing values with either given value, or `defaultValue`.
@@ -60,16 +63,20 @@ function methods:getPosition(index)
 end
 
 function methods:getIndex(x, y)
+  if type(x) == 'Nyoom.Vector2' then y, x = x.y, x.x end
+
   if x < 0 or x > self.width - 1 or y < 0 or y > self.height - 1 then return end
   return positionToIndex(x, y, self.width)
 end
 
 function methods:getValue(x, y)
+  if type(x) == 'Nyoom.Vector2' then y, x = x.y, x.x end
   local index = indexOrCoords(self, x, y)
   return self.map[index]
 end
 
 function methods:setValue(value, x, y)
+  if type(x) == 'Nyoom.Vector2' then y, x = x.y, x.x end
   local index = indexOrCoords(self, x, y)
   if index then self.map[index] = value end
   return self
